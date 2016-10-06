254e0447c09c6022a3511e8915b0c005e9a6e916
Neil Fenton <neilff@gmail.com>
2016-10-06 10:23:14 -0400

    added tests for rbac selector

--- exercises/02-saving-time-mock-api/rbac-selector.test.js
+++ exercises/02-saving-time-mock-api/rbac-selector.test.js
@@ -56,16 +56,96 @@ describe('02-saving-time', () => {
     //   },
     // };
 
+    // Don't forget to test the case of when no permissions object is provided
+    it('should return true if there are no permissions required', () => {
+      const expected = true;
+      const actual = rbacSelector(oldResponse, {});
+
+      expect(actual).to.equal(expected);
+    });
+
+    // Don't forget to test the case of when a poorly formed permissions object
+    // is provided
+    it('should return an error if a poor permissions object is provided', () => {
+      expect(() => rbacSelector(oldResponse, { allRequired: {} })).to.throw(TypeError);
+    });
+
     context('allRequired', () => {
-      // ...
+      it('should return true if the user has ALL the allRequired values', () => {
+        const expected = true;
+        const actual = rbacSelector(oldResponse, {
+          allRequired: [
+            ['account', 'canUpdate'],
+            ['expense', 'canUpdate'],
+          ],
+        });
+
+        expect(actual).to.equal(expected);
+      });
+
+      it('should return false if the user does not have ALL the allRequired values', () => {
+        const expected = false;
+        const actual = rbacSelector(oldResponse, {
+          allRequired: [
+            ['account', 'canCreate'],
+            ['expense', 'canCreate'],
+          ],
+        });
+
+        expect(actual).to.equal(expected);
+      });
     });
 
     context('notRequired', () => {
-      // ...
+      it('should return true if the user does NOT have ALL the notRequired values', () => {
+        const expected = true;
+        const actual = rbacSelector(oldResponse, {
+          notRequired: [
+            ['account', 'canCreate'],
+            ['expense', 'canCreate'],
+          ],
+        });
+
+        expect(actual).to.equal(expected);
+      });
+
+      it('should return false if the user does have ANY of the notRequired values', () => {
+        const expected = false;
+        const actual = rbacSelector(oldResponse, {
+          notRequired: [
+            ['account', 'canCreate'],
+            ['expense', 'canUpdate'],
+          ],
+        });
+
+        expect(actual).to.equal(expected);
+      });
     });
 
     context('anyRequired', () => {
-      // ...
+      it('should return true if the user has ANY of the anyRequired values', () => {
+        const expected = true;
+        const actual = rbacSelector(oldResponse, {
+          anyRequired: [
+            ['account', 'canUpdate'],
+            ['account', 'canCreate'],
+          ],
+        });
+
+        expect(actual).to.equal(expected);
+      });
+
+      it('should return false if the user does not have ANY the anyRequired values', () => {
+        const expected = false;
+        const actual = rbacSelector(oldResponse, {
+          anyRequired: [
+            ['account', 'canCreate'],
+            ['expense', 'canCreate'],
+          ],
+        });
+
+        expect(actual).to.equal(expected);
+      });
     });
   });
 });
